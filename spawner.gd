f8e54ce54bf156c87300b9b7b199a122093773e5
Jeffrey Berry <jeffreyberry93@gmail.com>
2026-01-12 22:02:41 -0800

    refactor(spawner.gd): remove unused _ready and _process functions

--- spawner.gd
+++ spawner.gd
@@ -2,16 +2,6 @@ extends Node
 
 @export var mob_scene: PackedScene
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_mob_timer_timeout() -> void:
 	# Create a new instance of the Mob scene
 	var mob = mob_scene.instantiate()
