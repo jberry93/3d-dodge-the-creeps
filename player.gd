b3bd278a7019f2a97e469db8611761310f112acc
Jeffrey Berry <jeffreyberry93@gmail.com>
2026-01-22 21:46:25 -0800

    feat(player.gd): adjust animation speed based on movement and refine character rotation

--- player.gd
+++ player.gd
@@ -38,6 +38,10 @@ func _physics_process(delta: float) -> void:
 		
 		# Setting the basis property will affect the rotation of the node
 		$Pivot.basis = Basis.looking_at(direction)
+		
+		$AnimationPlayer.speed_scale = 4
+	else:
+		$AnimationPlayer.speed_scale = 1
 	
 	# Ground velocity
 	target_velocity.x = direction.x * speed
@@ -80,6 +84,8 @@ func _physics_process(delta: float) -> void:
 	# Moving the character
 	velocity = target_velocity
 	move_and_slide()
+	
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
 
 func die():
 	hit.emit()
